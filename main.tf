9ec465958d8e47996bfc33ed6fa9531dc3778bf7
Badal.sahu <badal.sahu@ZL-BLR-MAC155.local>
2022-04-12 11:51:59 +0530

    one script

--- main.tf
+++ main.tf
@@ -1,3 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+provider "aws" {
+  region     = "us-west-2"
+  access_key = "AKIASAXNHVLHA2QJD7XY"
+  secret_key = "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"
+}
 resource "aws_s3_bucket" "bucket" {
   bucket = "my-tf-test-bucket"
   tags = {
