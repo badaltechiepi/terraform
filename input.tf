e84bb16e916d5b13930817cbd443ae288c909157
Badal.sahu <badal.sahu@ZL-BLR-MAC155.local>
2022-04-13 13:15:27 +0530

    the cidr range and the az zone

--- input.tf
+++ input.tf
@@ -2,11 +2,11 @@ variable "vpc_cidr" {
   default = "192.168.0.0/16"
   type    = string
 }
-variable "subnet_cidr" {
-  type    = list(string)
+variable "subnet_cidrs" {
   default = ["192.168.0.0/24","192.168.1.0/24","192.168.2.0/24","192.168.3.0/24"]
+  description = "This is the CIDR range of the subnets"
 }
-variable "subnet_avzone" {
-  type    = list(string)
+variable "subnet_azs" {
   default = ["web1","web2","db1","db2"]
+  description = "avalibility zone"
 }
