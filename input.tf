02c1a9f0b928e5996d4d283aa69d95f65c0ec5a9
Badal.sahu <badal.sahu@ZL-BLR-MAC155.local>
2022-04-13 13:20:35 +0530

    adding list

--- input.tf
+++ input.tf
@@ -3,10 +3,12 @@ variable "vpc_cidr" {
   type    = string
 }
 variable "subnet_cidrs" {
+  type = list(string)
   default = ["192.168.0.0/24","192.168.1.0/24","192.168.2.0/24","192.168.3.0/24"]
   description = "This is the CIDR range of the subnets"
 }
 variable "subnet_names" {
+  type    = list(string)
   default = ["web1","web2","db1","db2"]
   description = "avalibility zone"
 }
