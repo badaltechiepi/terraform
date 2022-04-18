02f93b70f1a0e4a9701a74d2a324c8f289cb7e17
Badal.sahu <badal.sahu@ZL-BLR-MAC155.local>
2022-04-18 17:42:23 +0530

    adding the security group

--- local.tf
+++ local.tf
@@ -1,4 +1,10 @@
 locals{
    
-    vpc_name =  "primary"
+    vpc_name  = "primary"
+    ssh_port  = 22
+    http_port = 80
+    https_port = 443
+    anywhere = "0.0.0.0/0"
+    protocol = "tcp"
+    web_sg    = "websg"
 }
--- network.tf
+++ network.tf
@@ -1,15 +1,56 @@
 resource "aws_vpc" "vpc" {
   cidr_block = var.vpc_cidr
-  tags = {
-    Name = local.vpc_name
+  tags       = {
+    Name     = local.vpc_name
   }
 }
+#subnet creation for the web,app,db
 resource "aws_subnet" "subnetes" {
-  count = 6
+  count      = 6
   vpc_id     = aws_vpc.vpc.id
   cidr_block = var.subnet_cidrs[count.index]
 
-  tags = {
-    Name = var.subnet_names[count.index]
+  tags       = {
+    Name     = var.subnet_names[count.index]
   }
 }
+#security group creation for the web
+resource "aws_security_group" "allow_tls" {
+  name        = "websg"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.vpc.id
+  ingress{
+  description = "ssh port for the web"
+  from_port   = local.ssh_port
+  to_port     = local.ssh_port
+  protocol    = local.protocol
+  cidr_blocks = [ local.anywhere ]
+  }
+  ingress{
+  description = "HTTP port for the web"
+  from_port   = local.http_port
+  to_port     = local.http_port
+  protocol    = local.protocol
+  cidr_blocks = [local.anywhere]
+  }
+  ingress{
+  description = "HTTPS port for the web"
+  from_port   = local.https_port
+  to_port     = local.https_port
+  protocol    = local.protocol
+  cidr_blocks = [local.anywhere]
+  }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = [local.anywhere]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = local.web_sg
+  }
+
+}
+
