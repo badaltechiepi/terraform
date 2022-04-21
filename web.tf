7c74e0a13a32181e4f586d36030111a42080585c
Badal.sahu <badal.sahu@ZL-BLR-MAC155.local>
2022-04-21 17:13:41 +0530

    fixed the ec2-instance{

--- web.tf
+++ web.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "web1" {
-  ami                         = "ami-0892d3c7ee96c0bf7"
+  ami                         = "ami-0fb653ca2d3203ac1" #Depend upon the aws configure setup in the terminal
   associate_public_ip_address = true
   instance_type               = "t2.micro"
   vpc_security_group_ids      = [aws_security_group.websg.id]
