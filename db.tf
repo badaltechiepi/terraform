9bb11fc189bc913c4645c1c3c989ca0bda6de66b
Badal.sahu <badal.sahu@ZL-BLR-MAC155.local>
2022-04-29 11:08:02 +0530

    adding the db and output file

---
+++ db.tf
@@ -0,0 +1,24 @@
+#creating the subnets group
+resource "aws_db_subnet_group" "db_subnet" {
+    name = "dbsubnet"
+    subnet_ids = [aws_subnet.subnets[2].id, aws_subnet.subnets[3].id]
+  tags = {
+    Name = "My DB subnet group"
+  }
+}
+#creating the RDS
+resource "aws_db_instance" "mypostgress" {
+    allocated_storage = 20
+    engine = "postgress"
+    apply_immediately = true
+    auto_minor_version_upgrade =  false
+    backup_retention_period = 0
+    db_db_subnet_group_name = aws_db_subnet_group.db_subnet
+    instance_class              = "db.t3.micro"
+    multi_az                    = false
+    name                        = "instacook"
+    username                    = "postgres"
+    password                    = "postgres" 
+    vpc_security_group_ids      = [aws_security_group.dbsg.id]
+
+}
---
+++ output.tf
@@ -0,0 +1,7 @@
+output "vpcid" {
+  value = aws_vpc.vpc
+}
+output "subnets" {
+    value = aws_subnet.subnets
+  
+}
