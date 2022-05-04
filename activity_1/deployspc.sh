42e03906cd326c337148dbf9291fb6e761264199
Badal.sahu <badal.sahu@ZL-BLR-MAC155.local>
2022-05-04 18:08:53 +0530

    adding the script as nginx

--- activity_1/deployspc.sh
+++ activity_1/deployspc.sh
@@ -1,8 +1,11 @@
+# #!/bin/bash
+# sudo apt update
+# sudo apt install openjdk-11-jdk -y
+# mkdir ~/apps 
+# cd ~/apps
+# wget https://referenceapplicationskhaja.s3.us-west-2.amazonaws.com/spring-petclinic-2.4.2.jar
+# java -jar spring-petclinic-2.4.2.jar &
+# sleep 240s
 #!/bin/bash
-sudo apt update
-sudo apt install openjdk-11-jdk -y
-mkdir ~/apps 
-cd ~/apps
-wget https://referenceapplicationskhaja.s3.us-west-2.amazonaws.com/spring-petclinic-2.4.2.jar
-java -jar spring-petclinic-2.4.2.jar &
-sleep 240s
+sudo apt update -y
+sudo apt install nginx -y
