328a08dd2f4f65b525e70e8d4632626674872761
Badal.sahu <badal.sahu@ZL-BLR-MAC155.local>
2022-05-04 10:40:42 +0530

    adding the condition whether to create the database of not

--- db.tf
+++ db.tf
@@ -1,26 +1,27 @@
-# #creating the subnets group
-# resource "aws_db_subnet_group" "db_subnet" {
-#     name = "dbsubnet"
-#     subnet_ids = [aws_subnet.subnets[2].id, aws_subnet.subnets[3].id]
-#   tags = {
-#     Name = "My DB subnet group"
-#   }
-# }
-# #creating the RDS
-# resource "aws_db_instance" "db" {
-#     allocated_storage           = 20
-#     engine                      = "postgres"
-#     apply_immediately           = true
-#     auto_minor_version_upgrade  =  false
-#     backup_retention_period     = 0
-#     db_subnet_group_name        = aws_db_subnet_group.db_subnet.name
-#     instance_class              = "db.t3.micro"
-#     multi_az                    = false
-#     name                        = "instacook"
-#     username                    = "postgres"
-#     password                    = "postgres" 
-#     vpc_security_group_ids      = [aws_security_group.dbsg.id]
-#     skip_final_snapshot         = true         #to delete the DB
+#creating the subnets group
+resource "aws_db_subnet_group" "db_subnet" {
+    name = "dbsubnet"
+    subnet_ids = [aws_subnet.subnets[2].id, aws_subnet.subnets[3].id]
+  tags = {
+    Name = "My DB subnet group"
+  }
+}
+#creating the RDS
+resource "aws_db_instance" "db" { 
+    count                       = var.db_creation == "yes"?1:0
+    allocated_storage           = 20
+    engine                      = "postgres"
+    apply_immediately           = true
+    auto_minor_version_upgrade  =  false
+    backup_retention_period     = 0
+    db_subnet_group_name        = aws_db_subnet_group.db_subnet.name
+    instance_class              = "db.t3.micro"
+    multi_az                    = false
+    name                        = "instacook"
+    username                    = "postgres"
+    password                    = "postgres" 
+    vpc_security_group_ids      = [aws_security_group.dbsg.id]
+    skip_final_snapshot         = true         #to delete the DB
 
 
-# }
+}
--- deployspc.sh
+++ deployspc.sh
@@ -5,4 +5,4 @@ mkdir ~/apps
 cd ~/apps
 wget https://referenceapplicationskhaja.s3.us-west-2.amazonaws.com/spring-petclinic-2.4.2.jar
 java -jar spring-petclinic-2.4.2.jar &
-sleep 120s
+sleep 240s
